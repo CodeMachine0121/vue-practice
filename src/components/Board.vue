480441cac756d4b382db77cdf8f5317ab53aa340
james.hsueh77 <james.hsueh@titansoft.com.sg>
2023-03-07 23:11:33 +0800

    feat: use v-for to transfer data

---
+++ src/components/Board.vue
@@ -0,0 +1,36 @@
+<script setup>
+const musicList = [
+  {title: "榜樣", score: 8, suggestion: true},
+  {title: "病變", score: 5, suggestion: false},
+  {title:"備胎", score: 7, suggestion: true}
+]
+</script>
+
+<template>
+<div class="whiteBoard">
+  <h1>{{title}}</h1>
+  <a v-bind:href="noteLink">this is my note Link which used v-bind </a>
+  <ul>
+    <li v-for="(music, index) in musicList" v-bind:key="music">
+      <input type="checkbox" v-bind:checked="music.suggestion">
+      {{index+1}}: {{music.title}} with score: {{music.score}}
+    </li>
+  </ul>
+
+</div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Board',
+  props:{
+    title: String,
+    noteLink: String,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <Board
+      title="Here for me to design vue"
+      noteLink="https://hackmd.io/@ksz54213/ryCbG3412"
+  />
+</template>
+
+<script>
+import Board from './components/Board.vue'
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "index",
+  components:{
+    Board
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
